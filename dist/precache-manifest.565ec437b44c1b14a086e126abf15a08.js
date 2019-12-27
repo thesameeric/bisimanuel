6925030c895e402a2fd1556834b74c98d75e597b
Osivwi Okiti <osivwiokiti@gmail.com>
2019-12-27 22:12:17 +0100

    UPDATED gitignore removing dist folder

---
+++ dist/precache-manifest.565ec437b44c1b14a086e126abf15a08.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf5bbdd77a9bec5aa58",
+    "url": "/css/account index.6094fcac.css"
+  },
+  {
+    "revision": "acf068199d0c4bc1e70e",
+    "url": "/css/account.4a6e8d01.css"
+  },
+  {
+    "revision": "fe187542c76588792207",
+    "url": "/css/app.523bf114.css"
+  },
+  {
+    "revision": "e0ba55c79946eba20b3b",
+    "url": "/css/details.9d40a2ad.css"
+  },
+  {
+    "revision": "0cf7b19e46d80b700d5f80469031c47d",
+    "url": "/css/eric.css"
+  },
+  {
+    "revision": "c4877cdead843645da5b",
+    "url": "/css/login.61773db8.css"
+  },
+  {
+    "revision": "ca0e3e5576b79a5c57b0218b087b9526",
+    "url": "/img/concert.ca0e3e55.jpg"
+  },
+  {
+    "revision": "b18f4f9c96f5ab3f5caebe8a78b99a0e",
+    "url": "/img/drake.b18f4f9c.jpg"
+  },
+  {
+    "revision": "aa9a4c7bee00042370c2bf5cbacd42dd",
+    "url": "/img/logo.aa9a4c7b.png"
+  },
+  {
+    "revision": "19e0863365741970d8eaace234a72b63",
+    "url": "/img/team-2.19e08633.jpg"
+  },
+  {
+    "revision": "f35a97b32bb42a8ae55aa524441d898c",
+    "url": "/img/university.f35a97b3.svg"
+  },
+  {
+    "revision": "426595d60763a1be872f0dd246424481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d935cff0785d8fe4d8",
+    "url": "/js/about.53b71bc3.js"
+  },
+  {
+    "revision": "bcf5bbdd77a9bec5aa58",
+    "url": "/js/account index.8a83a0d3.js"
+  },
+  {
+    "revision": "acf068199d0c4bc1e70e",
+    "url": "/js/account.ee33975b.js"
+  },
+  {
+    "revision": "fe187542c76588792207",
+    "url": "/js/app.ecea2bd3.js"
+  },
+  {
+    "revision": "c704909d83c4ccd5070e",
+    "url": "/js/chunk-vendors.f7978236.js"
+  },
+  {
+    "revision": "e0ba55c79946eba20b3b",
+    "url": "/js/details.76c7dc73.js"
+  },
+  {
+    "revision": "c4877cdead843645da5b",
+    "url": "/js/login.7afd91a9.js"
+  },
+  {
+    "revision": "4bf4d136569fb5f04924b8d303a8fe25",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
